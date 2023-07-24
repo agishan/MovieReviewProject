8c59f3b055dea01fc2b2e3b2e0fefd14f02c0927
agishan <54005039+agishan@users.noreply.github.com>
2023-07-24 11:46:31 -0400

    old code

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,18 +1,12 @@
-import * as React from 'react';
-import Review from './Review';
-
+import * as React from "react";
+import Review from "./Review";
 
 const App = () => {
-
-
   return (
     <div>
-        <h1>MSci 245 - D1 template </h1>
-      {/* Render <Review /> child component */}
-
-
+      <Review />
     </div>
   );
-}
+};
 
 export default App;
